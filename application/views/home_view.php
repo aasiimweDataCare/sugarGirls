b4235ff429900db1ecf7efc973217d60d2995496
Asiimwe Apollo <xarafire@gmail.com>
2017-03-02 01:13:00 +0300

    SugarGirls After three qtr of home page branded

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Online Dating and Travelling Site">
     <meta name="keywords" content="Dating,Travelling,Girls,Sugar">
     <meta name="author" content="ComeEasy, ICT,Solutions, Uganda, Asiimwe, Apollo,">
-    <link rel="icon" href="<?= base_url() ?>assets/images/favicon.png">
+    <link rel="icon" href="<?= base_url() ?>assets/images/favicon.PNG">
     <link href="<?= base_url() ?>dist/css/nta.css" media="all" rel="stylesheet" type="text/css">
     <title><?= projectName; ?>:Home</title>
     <link href="<?= base_url() ?>bootstrap-3.3.5/css/bootstrap.min.css" rel="stylesheet">
@@ -81,38 +81,56 @@
 <div class="clearfix"></div>
 <div class="col-sm-12 grey-container">
     <div class="col-sm-4">
-        <img src="<?= base_url() ?>assets/images/3PhotosStacked.png" width="70%" height="70%" class="rounded float-left"
+        <img src="<?= base_url() ?>assets/images/3PhotosStacked.png" width="100%" height="80%"
+             class="rounded float-left"
              alt="...">
     </div>
     <div class="col-sm-4">
         <div class="panel">
             <a href="<?= site_url('MdLoans/mortgages_home'); ?>">
                 <div class="panel-body">
+                    <div class="col-sm-4">
                         <span class="fa-stack fa-lg">
                           <i class="fa fa-circle fa-stack-2x icon-background1"></i>
-                          <i class="fa fa-male fa-stack-1x"></i>
+                          <i class="fa fa-male fa-stack-1x icon-background2"></i>
                         </span>
-
-                    <div class="text-right">
-                        <div class="huge">615,375</div>
-                        <div style="white-space: nowrap;">Members on Sugar Girls</div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="text-left">
+                            <div class="huge">
+                                <span class="count">615375</span>
+                            </div>
+                            <div style="white-space: nowrap;">Members on Sugar Girls</div>
+                            &nbsp;&nbsp;&nbsp;<br/>
+                            &nbsp;&nbsp;&nbsp;<br/>
+                            &nbsp;&nbsp;&nbsp;<br/>
+                        </div>
                     </div>
                 </div>
             </a>
         </div>
     </div>
+
     <div class="col-sm-4">
         <div class="panel">
             <a href="<?= site_url('MdLoans/mortgages_home'); ?>">
                 <div class="panel-body">
+                    <div class="col-sm-4">
                         <span class="fa-stack fa-lg">
                           <i class="fa fa-circle fa-stack-2x icon-background1"></i>
-                          <i class="fa fa-male fa-stack-1x"></i>
+                          <i class="fa fa-plane fa-stack-1x icon-background2"></i>
                         </span>
-
-                    <div class="text-right">
-                        <div class="huge">324,780</div>
-                        <div style="white-space: nowrap;">Total Trips created</div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="text-left">
+                            <div class="huge">
+                                <span class="count">324831</span>
+                            </div>
+                            <div style="white-space: nowrap;">Total Trips created</div>
+                            &nbsp;&nbsp;&nbsp;<br/>
+                            &nbsp;&nbsp;&nbsp;<br/>
+                            &nbsp;&nbsp;&nbsp;<br/>
+                        </div>
                     </div>
                 </div>
             </a>
@@ -121,86 +139,163 @@
 </div>
 <div class="clearfix"></div>
 <div class="col-sm-12 white-container">
-    <div class="col-sm-8 col-sm-offset-2">
-        "Unlike other dating websites, it's my commitment to operate our business ethically. This means caring about
-        your privacy, encrypting identifiable data, and never employing fake profiles or software bots. When we say
-        there are more women than men, we guarantee it."
+    <br/>
 
-        Brandon Wade
-        Founder & CEO, SugarGirls.com
-        Instagram 7.9K people are following Brandon Wade.
+    <div class="col-sm-9 col-sm-offset-1">
+        <div class="col-sm-2">
+            <img src="<?= base_url() ?>assets/images/ceo.PNG" width="100%" height="100%"
+                 class="rounded float-left img-thumbnail"
+                 alt="Word From CEO">
+        </div>
+        <div class="col-sm-7">
+            <p class="text-left">"Unlike other dating websites, it's my commitment to operate our business ethically.
+                This
+                means caring about
+                your privacy, encrypting identifiable data, and never employing fake profiles or software bots. When we
+                say
+                there are more women than men, we guarantee it."<br/>
+            <hr>
+            Andre Mitimituje<br/>
+            Founder & CEO, SugarGirls.com<br/>
+            Instagram 7.9K people are following Andre Mitimituje.
+            </p>
+        </div>
     </div>
 </div>
 <div class="clearfix"></div>
 <div class="col-sm-12 grey-container">
     <div class="col-sm-3 col-sm-offset-1">
-        <div class="panel panel-primary">
+        <div class="panel panel-primary panel-transparent">
             <div class="panel-heading">
                 <h3 class="panel-title">Popular upcoming trips</h3>
-                    <span class="pull-right">
-                        <!-- Tabs -->
-                        <ul class="nav panel-tabs">
-                            <li class="active"><a href="#tab1" data-toggle="tab">Tab 1</a></li>
-                            <li><a href="#tab2" data-toggle="tab">Tab 2</a></li>
-                            <li><a href="#tab3" data-toggle="tab">Tab 3</a></li>
-                            <li><a href="#tab4" data-toggle="tab">Tab 4</a></li>
-                        </ul>
-                    </span>
             </div>
             <div class="panel-body">
-                <div class="tab-content">
-                    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
-                    illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui
-                    blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
-                    dolor sit amet,
+                <div class="clearfix"></div>
+                <div class="row well white-container">
+                    <div class="col-sm-4">
+                        <img src="<?= base_url() ?>assets/images/ceo.PNG" width="500%" height="500%"
+                             class="rounded float-left img-thumbnail"
+                             alt="Word From CEO">
+                    </div>
+                    <div class="col-sm-8">
+                        upintheairguy<br/>
+
+                        <b>Wants to travel to:</b><br/>
+                        Honolulu, United States<br/>
+                        <a href="#">View Trip Details</a> <i class="fa fa-angle-double-right" aria-hidden="true"></i>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+                <div class="row well white-container">
+                    <div class="col-sm-4">
+                        <img src="<?= base_url() ?>assets/images/ceo.PNG" width="500%" height="500%"
+                             class="rounded float-left img-thumbnail"
+                             alt="Word From CEO">
+                    </div>
+                    <div class="col-sm-8">
+                        upintheairguy<br/>
+
+                        <b>Wants to travel to:</b><br/>
+                        Honolulu, United States<br/>
+                        <a href="#">View Trip Details</a> <i class="fa fa-angle-double-right" aria-hidden="true"></i>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+                <div class="row well white-container">
+                    <div class="col-sm-4">
+                        <img src="<?= base_url() ?>assets/images/ceo.PNG" width="500%" height="500%"
+                             class="rounded float-left img-thumbnail"
+                             alt="Word From CEO">
+                    </div>
+                    <div class="col-sm-8">
+                        upintheairguy<br/>
+
+                        <b>Wants to travel to:</b><br/>
+                        Honolulu, United States<br/>
+                        <a href="#">View Trip Details</a> <i class="fa fa-angle-double-right" aria-hidden="true"></i>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="col-sm-3">
-        <div class="panel panel-primary">
+        <div class="panel panel-primary panel-transparent">
             <div class="panel-heading">
                 <h3 class="panel-title">Top 10 Destinations</h3>
-                    <span class="pull-right">
-                        <!-- Tabs -->
-                        <ul class="nav panel-tabs">
-                            <li class="active"><a href="#tab1" data-toggle="tab">Tab 1</a></li>
-                            <li><a href="#tab2" data-toggle="tab">Tab 2</a></li>
-                            <li><a href="#tab3" data-toggle="tab">Tab 3</a></li>
-                            <li><a href="#tab4" data-toggle="tab">Tab 4</a></li>
-                        </ul>
-                    </span>
             </div>
             <div class="panel-body">
-                <div class="tab-content">
-                    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
-                    illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui
-                    blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
-                    dolor sit amet,
-                </div>
+                <ol>
+                    <li><a href="#">Las Vegas, United States</a></li>
+                    <li><a href="#">Bali, Indonesia</a></li>
+                    <li><a href="#">London, United Kingdom</a></li>
+                    <li><a href="#">Miami, United States</a></li>
+                    <li><a href="#">Cancun, Mexico</a></li>
+                    <li><a href="#">New York, New York</a></li>
+                    <li><a href="#">Paris, France</a></li>
+                    <li><a href="#">Dubai, UAE</a></li>
+                    <li><a href="#">Los Angeles, CA</a></li>
+                    <li><a href="#">San Francisco, CA</a></li>
+                </ol>
             </div>
         </div>
     </div>
     <div class="col-sm-4">
-        <div class="panel panel-primary">
+        <div class="panel panel-primary panel-transparent">
             <div class="panel-heading">
                 <h3 class="panel-title">Register for free today!</h3>
-                    <span class="pull-right">
-                        <!-- Tabs -->
-                        <ul class="nav panel-tabs">
-                            <li class="active"><a href="#tab1" data-toggle="tab">Tab 1</a></li>
-                            <li><a href="#tab2" data-toggle="tab">Tab 2</a></li>
-                            <li><a href="#tab3" data-toggle="tab">Tab 3</a></li>
-                            <li><a href="#tab4" data-toggle="tab">Tab 4</a></li>
-                        </ul>
-                    </span>
             </div>
             <div class="panel-body">
-                <div class="tab-content">
-                    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
-                    illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui
-                    blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
-                    dolor sit amet,
+                <div class="row well white-container">
+                    <div class="col-sm-8 col-sm-offset-2 container-fluid custom-forms">
+                        <?php
+                        $attributes = array("class" => "", "id" => "sg_register_form", "name" => "sg_register_form");
+                        echo form_open("SgHome/register_submit", $attributes);
+                        ?>
+
+                        <?= $this->session->flashdata('msg_sg_register'); ?>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="username" name="username"
+                                   value="<?= set_value('username'); ?>"
+                                   required="required" title="Please enter your desired username"
+                                   placeholder="Username"/>
+                            <span class="text-danger"><?= form_error('username'); ?></span>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="email_add" name="email_add"
+                                   value="<?= set_value('email_add'); ?>"
+                                   required="required" title="Please enter your email address"
+                                   placeholder="Email Address"/>
+                            <span class="text-danger"><?= form_error('email_add'); ?></span></div>
+
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password" name="password"
+                                   value="<?= set_value('password'); ?>"
+                                   required="required" title="Please enter your desired Password"
+                                   placeholder="Password"/>
+                            <span class="text-danger"><?= form_error('password'); ?></span>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="checkbox" id="privacy_policy" name="privacy_policy" value="1">
+                            By creating an account, you agree to our
+                            <a>Terms</a>
+                            and
+                            <a>Privacy Policy</a>
+                            <span class="text-danger"><?= form_error('privacy_policy'); ?></span>
+                        </div>
+
+                        <input type="submit" name="register_user" id="register_user" value="Create Account"
+                               class="btn btn-success custom-submit-btn"/>
+                        <br/>
+                        <?= form_close(); ?>
+                        <?= $this->session->flashdata('msg_sg_register'); ?>
+                        <p class="footer">Please <a href="#" data-toggle="modal" data-target="#loginModal">Log
+                                In</a> if you already have an account</p>
+
+                    </div>
                 </div>
             </div>
         </div>
@@ -208,13 +303,42 @@
 </div>
 <div class="clearfix"></div>
 <div class="col-sm-12 dark-grey-container">
-    <h2>SugarGirls Blog</h2>
+    <div class="panel panel-primary panel-transparent">
+        <div class="panel-heading">
+            <h3 class="panel-title light-header">Sugar Girls Blog</h3>
+        </div>
+        <div class="panel-body">
+            <div class="row well purple-background">
+                <div class="col-sm-12">
+                    <p class="text-left">
 
-    <div>
-        <h3>5 Top Tips for Traveling to London</h3>
-    </div>
-    <div>
-        <h3>A Romantic Trip to London</h3>
+                        <img src="<?= base_url() ?>assets/images/blog/pic1.png" width="15%" height="15%"
+                             class="rounded float-left"
+                             alt="blog-1">
+                        Traditionally, Mardi Gras is celebrated in Louisiana. The party kicks off two
+                        weeks before Shrove Tuesday, or Fat Tuesday. You won&#146;t want to miss this annual opportunity
+                        to party without inhibition. Major parades are held each day and the most elaborate of these are
+                        during the last days of the holiday. Parades are not the [&hellip;] [&nbsp;<a
+                            href="#" target="_blank">read more </a>&nbsp;]
+                    <ul>
+                        <li>February 28, 2017</li>
+                         
+                        <li>EthanMT</li>
+                    </ul>
+                    </p>
+                </div>
+            </div>
+            <div class="row well purple-background">
+                <div class="col-sm-12">
+                    <p class="text-left">
+                        <img src="<?= base_url() ?>assets/images/blog/pic2.png" width="15%" height="15%"
+                             class="rounded float-left"
+                             alt="Word From CEO">
+                        ggg
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <div class="clearfix"></div>
@@ -237,7 +361,7 @@
                 line-height: 21px;
                 color: #fff;
                 font-size: 12px;
-                background: url('<?php echo base_url(); ?>assets/images/slideshow/b03.png') no-repeat;
+                background: url('<?php echo base_url(); ?>assets/images/slideshow/b03.PNG') no-repeat;
                 overflow: hidden;
                 cursor: pointer
             }
@@ -264,7 +388,7 @@
                 width: 55px;
                 height: 55px;
                 cursor: pointer;
-                background: url('<?php echo base_url(); ?>assets/images/slideshow/a03.png') no-repeat;
+                background: url('<?php echo base_url(); ?>assets/images/slideshow/a03.PNG') no-repeat;
                 overflow: hidden
             }
 
@@ -450,7 +574,7 @@
 </div>
 <div class="clearfix"></div>
 <div align="center" class="row white-container">
-    <div class="col-xs-18 col-md-12">
+    <div class="col-sm-18 col-md-12">
         <img class="img-fade" src="<?php echo base_url() ?>assets/images/logo_footer.png" width="30%" height="30%"
              alt="<?= projectName; ?>-logo">
         <footer>
@@ -465,6 +589,7 @@
 </div>
 <script src="<?= base_url() ?>bootstrap-3.3.5/js/jquery-1.11.3.min.js"></script>
 <script src="<?= base_url() ?>bootstrap-3.3.5/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url() ?>dist/js/countdown.js"></script>
 <script src="<?php echo base_url(); ?>dist/js/slideshow.js"></script>
 <script type="text/javascript">jssor_1_slider_init();</script>
 <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel">
@@ -476,7 +601,7 @@
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-xs-12 custom-forms">
+                    <div class="col-sm-12 custom-forms">
                         <?php
                         $attributes = array("class" => "", "id" => "loginform", "name" => "loginform");
                         echo form_open("SgHome/index", $attributes); ?>
